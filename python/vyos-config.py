eff2191badf6a4e268baadcd743e399e526edfd3
Masanori Itoh <masanori.itoh@gmail.com>
2016-04-08 01:55:18 +0900

    Refactor python class system.

--- python/vyos-config.py
+++ python/vyos-config.py
@@ -1,143 +1,141 @@
 #!/usr/bin/python
 # 
-# vyatta1.py : An example to work with vyatta using pexpect.
+# vyos-config.py : An example to work with vyos using pexpect.
 #
-# This script is based on an example script, monitor.py,
-# contained in 'pexpect-2.3.tar.gz'.
+# This script is based on an example script, monitor.py, contained in 'pexpect-2.3.tar.gz'.
+# 
+# Author: Masanori Itoh <masanori.itoh@gmail.com>
 #
 import sys
 import pexpect
 
-host = 'HOSTNAME_OR_IPADDRESS'
-user = 'vyatta' # Usually no need to rewrite.
-password = 'PASSWORD'
+class VyosConfig(object):
+    host = ''
+    user = 'vyos'
+    password = ''
+    hostname = 'vyos'
+    
+    child = None
 
-COMMAND_PROMPT = '[#$] ' ### This is way too simple for industrial use -- we will change is ASAP.
-TERMINAL_PROMPT = '(?i)terminal type\?'
-TERMINAL_TYPE = 'vt100'
-# This is the prompt we get if SSH does not have the remote host's public key stored in the cache.
-SSH_NEWKEY = '(?i)are you sure you want to continue connecting'
+    COMMAND_PROMPT = '[#$] '
+    TERMINAL_PROMPT = '(?i)terminal type\?'
+    TERMINAL_TYPE = 'vt100'
+    # This is the prompt we get if SSH does not have the remote host's public key stored in the cache.
+    SSH_NEWKEY = '(?i)are you sure you want to continue connecting'
 
-def config_main():
-    global COMMAND_PROMPT, TERMINAL_PROMPT, TERMINAL_TYPE, SSH_NEWKEY
-    #
-    # Login via SSH
-    #
-    child = pexpect.spawn('ssh -l %s %s'%(user, host))
-    i = child.expect([pexpect.TIMEOUT, SSH_NEWKEY, COMMAND_PROMPT, '(?i)password'])
-    if i == 0: # Timeout
-        print 'ERROR! could not login with SSH. Here is what SSH said:'
-        print child.before, child.after
-        print str(child)
-        sys.exit (1)
-    if i == 1: # In this case SSH does not have the public key cached.
-        child.sendline ('yes')
-        child.expect ('(?i)password')
-    if i == 2:
-        # This may happen if a public key was setup to automatically login.
-        # But beware, the COMMAND_PROMPT at this point is very trivial and
-        # could be fooled by some output in the MOTD or login message.
-        pass
-    if i == 3:
-        child.sendline(password)
-        # Now we are either at the command prompt or
-        # the login process is asking for our terminal type.
-        i = child.expect ([COMMAND_PROMPT, TERMINAL_PROMPT])
-        if i == 1:
-            child.sendline (TERMINAL_TYPE)
-            child.expect (COMMAND_PROMPT)
-    #
-    # Set command prompt to something more unique.
-    #
-    COMMAND_PROMPT = "\[PEXPECT\]\$ "
-    child.sendline ("PS1='[PEXPECT]\$ '") # In case of sh-style
-    i = child.expect ([pexpect.TIMEOUT, COMMAND_PROMPT], timeout=10)
-    if i == 0:
-        print "# Couldn't set sh-style prompt -- trying csh-style."
-        child.sendline ("set prompt='[PEXPECT]\$ '")
-        i = child.expect ([pexpect.TIMEOUT, COMMAND_PROMPT], timeout=10)
-        if i == 0:
-            print "Failed to set command prompt using sh or csh style."
-            print "Response was:"
-            print child.before
+    def __init__(self, host, user, password=''):
+        self.host = host
+        self.user = user
+        self.password = password
+
+    def set_host(self, host):
+        self.host = host
+
+    def set_user(self, user):
+        self.user = user
+
+    def set_password(self, password):
+        self.password = password
+
+    def set_command_prompt(self, prompt):
+        self.COMMAND_PROMPT = prompt
+
+    def set_terminal_prompt(self, prompt):
+        self.TERMINAL_PROMPT = prompt
+
+    def login(self):
+
+        self.child = pexpect.spawn('ssh -l %s %s'%(self.user, self.host))
+        i = self.child.expect([pexpect.TIMEOUT, self.SSH_NEWKEY, self.COMMAND_PROMPT, '(?i)password'])
+        if i == 0: # Timeout
+            print 'ERROR! could not login with SSH. Here is what SSH said:'
+            print self.child.before, self.child.after
+            print str(self.child)
             sys.exit (1)
+            # In this case SSH does not have the public key cached.
+        if i == 1:
+            self.child.sendline ('yes')
+            self.child.expect ('(?i)password')
+        if i == 2:
+            # This may happen if a public key was setup to 
+            # automatically login.
+            # But beware, the COMMAND_PROMPT at this point is 
+            # very trivial and could be fooled by some output
+            # in the MOTD or login message.
+            pass
+        if i == 3:
+            self.child.sendline(self.password)
+            # Now we are either at the command prompt or
+            # the login process is asking for our terminal type.
+            i = self.child.expect ([self.COMMAND_PROMPT, self.TERMINAL_PROMPT])
+            if i == 1:
+                self.child.sendline (self.TERMINAL_TYPE)
+                self.child.expect (self.COMMAND_PROMPT)
+        #
+        # Set command prompt to something more unique.
+        #
+        COMMAND_PROMPT = "\[PEXPECT\]\$ "
+        self.child.sendline ("PS1='[PEXPECT]\$ '") # In case of sh-style
+        i = self.child.expect ([pexpect.TIMEOUT, self.COMMAND_PROMPT], timeout=10)
+        if i == 0:
+            print "# Couldn't set sh-style prompt -- trying csh-style."
+            self.child.sendline ("set prompt='[PEXPECT]\$ '")
+            i = self.child.expect ([pexpect.TIMEOUT, self.COMMAND_PROMPT], timeout=10)
+            if i == 0:
+                print "Failed to set command prompt using sh or csh style."
+                print "Response was:"
+                print self.child.before
+                sys.exit (1)
+                
+    def cmd(self, cmd='', debug=False):
+        self.child.sendline (cmd)
+        self.child.expect (self.COMMAND_PROMPT)
+        if debug:
+            print self.child.before
 
-    #
-    # Debug: show uname
-    #
-    child.sendline ('uname -a')
-    child.expect (COMMAND_PROMPT)
-    print child.before
+    def configure(self):
+        self.set_command_prompt(self.user + '@' + self.hostname + '# ')
+        self.cmd('configure')
+        # disable pager for automation
+        self.cmd('export VYATTA_PAGER=')
 
-    #
-    # Debug: tty information
-    #
-    child.sendline ('tty')
-    child.expect (COMMAND_PROMPT)
-    print child.before
+    def commit(self):
+        self.cmd('commit')
 
-    #
-    # Example: Get configuration mode. (IMPORANT)
-    #
-    # change prompt string at first. (work around)
-    # Note: 'vyatta' of '@vyatta' should be re-written.
-    COMMAND_PROMPT = "vyatta@vyatta# "
-    child.sendline ('configure')
-    child.expect (COMMAND_PROMPT)
-    print child.before
+    def save(self):
+        self.cmd('save')
 
-    #
-    # Example: suppress pager to automation. (IMPORANT)
-    #
-    child.sendline ('export VYATTA_PAGER=')
-    child.expect (COMMAND_PROMPT)
-    print child.before
+    def logout(self):
+        self.cmd('exit')
+        self.set_command_prompt(self.user + '@' + self.hostname + ':~$ ')
+        self.cmd('exit')
+        sys.exit(0)
 
-    #
-    # Example: show ntp servers, and show multi lines result so that
-    #          we can parse it and handle them appropriately.
-    child.sendline ('show system ntp server')
-    child.expect (COMMAND_PROMPT)
-    result = child.before
-    print result
-    # we get '^M' (\r) in each line, so throw it away.
-    result = result.replace('\r', '')
-    for l in result.split('\n'):
-        print '  ', l
 
-    # Initlally, vyatta installation refeers (1|2|3).vyatta.pool.ntp.org
-    child.sendline ('delete system ntp server 2.vyatta.pool.ntp.org')
-    child.expect (COMMAND_PROMPT)
-    print child.before
-    child.sendline ('delete system ntp server 1.vyatta.pool.ntp.org')
-    child.expect (COMMAND_PROMPT)
-    print child.before
-    child.sendline ('delete system ntp server 0.vyatta.pool.ntp.org')
-    child.expect (COMMAND_PROMPT)
-    print child.before
+if __name__ == '__main__':
+    vyos = VyosConfig('192.168.31.129', 'vyos', 'vyos')
+    vyos.login()
+    vyos.configure()
     #
-    # Example: show the result
+    # example: show all configuration
     #
-    child.sendline ('show system ntp server')
-    child.expect (COMMAND_PROMPT)
-    print child.before
+    vyos.cmd('show', debug=True)
+    #
+    # example: set IP address and default route
+    #
+    #vyos.cmd('set interfaces ethernet eth1 address 10.0.0.123/24')
+    #vyos.cmd('set protocols static route 0.0.0.0/0 next-hop 10.0.0.1 distance 1')
+    #
+    # example: reconfigure ntp server
+    #
+    #vyos.cmd('delete ntp')
+    #vyos.cmd('set ntp server ' + '10.0.0.2')
+    #
+    # example: enable ssh service on tcp 22
+    #
+    #vyos.cmd('set service ssh port 22')
 
-    #
-    # Example: save your changes (IMPORANT)
-    #
-    child.sendline ('save')
-    child.expect (COMMAND_PROMPT)
-    print child.before
+    #vyos.logout()
 
-    #
-    # Example: commit your changes (IMPORANT)
-    #
-    child.sendline ('commit')
-    child.expect (COMMAND_PROMPT)
-    print child.before
-#
-#
-#
-config_main()
-
+    sys.exit(0)
 
