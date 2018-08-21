76f1dd9167089171dbc5d74438522db6edf4a047
Masanori Itoh <masanori.itoh@gmail.com>
2018-08-22 00:25:14 +0900

    Cleanup nf_skelton.c

--- c/nf_skelton/nf_skelton.c
+++ c/nf_skelton/nf_skelton.c
@@ -40,28 +40,20 @@ MODULE_DESCRIPTION("A simple module for network trouble shooting.");
 MODULE_LICENSE("GPL");
 MODULE_VERSION("1.0");
 
-static int flags = 0;
-module_param(flags, int, 0644);
-MODULE_PARM_DESC(flags, "TCP flags (SYN,RST, etc.)");
 
-static int hooks = NF_INET_FORWARD;
-module_param(hooks, int, 0644);
-MODULE_PARM_DESC(hooks, "hooks, netfilter hook points");
+static int hook = NF_INET_PRE_ROUTING;
+module_param(hook, int, 0644);
+MODULE_PARM_DESC(hooks, "hook, netfilter hook points");
 
 static int verbose = 2;
 module_param(verbose, int, 0644);
 MODULE_PARM_DESC(verbose, "verbose mode");
 
-static int action = 0;
-module_param(action, int, 0644);
-MODULE_PARM_DESC(action, "action, 0: dump_stack(), 1: panic()");
-
-
 static unsigned int nf_skelton_net_id __read_mostly;
 
 static unsigned int nf_skelton_hook(void *priv,
-				struct sk_buff *skb,
-					const struct nf_hook_state *state)
+				    struct sk_buff *skb,
+				    const struct nf_hook_state *state)
 {
 	if (verbose >= 2) {
 		printk("%s: called: %p %p %p\n",
@@ -74,7 +66,7 @@ static unsigned int nf_skelton_hook(void *priv,
 static struct nf_hook_ops nf_skelton_hook_ops = {
 	.hook     = nf_skelton_hook,
 	.pf       = NFPROTO_IPV4,
-	.hooknum  = NF_INET_PRE_ROUTING,
+	.hooknum  = 0,
 	.priority = 100,
 };
 
@@ -89,8 +81,11 @@ static int __net_init __nf_skelton_init(struct net *net)
 	printk("%s: called: \n", __FUNCTION__);
 	
 	ptr = net_generic(net, nf_skelton_net_id);
-	printk("%s: %d %p \n", __FUNCTION__, nf_skelton_net_id, ptr);
-	
+	printk("%s: nf_skelton_id=%d ptr=%p \n", __FUNCTION__,
+	       nf_skelton_net_id, ptr);
+	/* FIXME: failure case ? */
+
+	nf_skelton_hook_ops.hooknum = hook;
 	ret = nf_register_net_hook(net, &nf_skelton_hook_ops);
 	if (ret < 0) {
 		return -ENOMEM;
@@ -126,31 +121,29 @@ static int __init nf_skelton_init(void)
 {
 	int ret;
 
-	printk("%s: called\n", __FUNCTION__);
-
-	printk("%s: flags=%08x, action=%d, verbose=%d\n",
-	       __FUNCTION__, flags, action, verbose);
-
+	printk("%s: called. hook=%d verbose=%d\n", __FUNCTION__,
+	       hook, verbose);
 
 	ret = register_pernet_subsys(&nf_skelton_ops);
-	//ret = nf_register_hook(&nf_skelton_hook_ops);
 	if (ret < 0) {
 		printk("%s: failed to register nf_skelton_ops\n",
 		       __FUNCTION__);
-		goto bailout_post_routing;
+		goto bailout_subsys;
 
 	}
 	ret = register_pernet_device(&nf_skelton_dev_ops);
 	if (ret < 0) {
 		printk("%s: failed to register nf_skelton_dev_ops\n",
 		       __FUNCTION__);
-		unregister_pernet_subsys(&nf_skelton_ops);
-		goto bailout_post_routing;
+		goto bailout_dev;
 	}
 	
 	return 0;
 
-  bailout_post_routing:
+bailout_dev:
+	unregister_pernet_subsys(&nf_skelton_ops);
+
+bailout_subsys:
 
 	return -ENOMEM;
 }
