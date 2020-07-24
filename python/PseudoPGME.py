6d82e10496f29b75c7dc64abeb4dbe79c5729111
Masanori Itoh <masanori.itoh@gmail.com>
2020-07-24 16:23:57 +0900

    Add PseudoPGME.py

---
+++ python/PseudoPGME.py
@@ -0,0 +1,82 @@
+#
+# PseudoPGME : Yet another pgme running with just python3 and nvidia-smi.
+#
+# Description:
+#   This script returns with compatible output with  pgme
+#   (Prometheus GPU Metrics Exporter) available at:
+#     https://github.com/chhibber/pgme
+#
+# History:
+#   * 2020/07/24 v0.1 Initial version
+# Authour:
+#   Masanori Itoh <masanori.itoh@gmail.com>
+#
+import sys, os, getopt, errno
+import subprocess
+
+from http.server import BaseHTTPRequestHandler
+from http.server import HTTPServer
+from http import HTTPStatus
+
+command =  ['nvidia-smi',
+            '--query-gpu=name,index,temperature.gpu,utilization.gpu,utilization.memory,memory.total,memory.free,memory.used',
+            '--format=csv,noheader,nounits']
+#
+#
+#
+class PseudoPGME(BaseHTTPRequestHandler):
+
+    def do_GET(self):
+
+        self.send_response(HTTPStatus.OK)
+        self.send_header('Content-Type', 'text/plain')
+        self.end_headers()
+
+        output = subprocess.Popen(command, stdin=subprocess.PIPE,
+                                  stdout=subprocess.PIPE,
+                                  close_fds=True).stdout
+        self.parse_nvidia_smi(output)
+
+    def parse_nvidia_smi(self, output):
+
+        for line in output:
+            l = line.strip()
+            if len(l) < 1:
+                continue
+
+            (gpu_name, gpu_index,
+             gpu_temp, gpu_util,
+             mem_util, mem_total,
+             mem_free, mem_used) = l.decode('utf-8').split(',')
+
+            gpu_index = int(gpu_index)
+            self.wfile.write(("temperature_gpu{gpu=\"%s[%d]\"} %s\n" % (gpu_name, gpu_index, gpu_temp)).encode('utf-8'))
+            self.wfile.write(("utilization_gpu{gpu=\"%s[%d]\"} %s\n" % (gpu_name, gpu_index, gpu_util)).encode('utf-8'))
+            self.wfile.write(("utilization_memory{gpu=\"%s[%d]\"} %s\n" % (gpu_name, gpu_index, mem_util)).encode('utf-8'))
+            self.wfile.write(("memory_total{gpu=\"%s[%d]\"} %s\n" % (gpu_name, gpu_index, mem_total)).encode('utf-8'))
+            self.wfile.write(("memory_free{gpu=\"%s[%d]\"} %s\n" % (gpu_name, gpu_index, mem_free)).encode('utf-8'))
+            self.wfile.write(("memory_used{gpu=\"%s[%d]\"} %s\n" % (gpu_name, gpu_index, mem_used)).encode('utf-8'))
+        return
+#
+#
+#
+if __name__ == "__main__":
+
+    bind_address = '0.0.0.0'
+    port = 18080
+
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], "p:b:")
+    except getopt.GetoptError as err:
+        print(str(err))
+        sys.exit(2)
+
+    for o, a in opts:
+        if o == '-p':
+            port = int(a)
+        elif o == '-b':
+            bind_address = a
+
+    httpd = HTTPServer((bind_address, port), PseudoPGME)
+
+    httpd.serve_forever()
