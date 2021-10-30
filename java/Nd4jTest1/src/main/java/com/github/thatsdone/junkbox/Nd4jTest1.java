10e6a17e420901e758822c8c5d89a02014edb724
Masanori Itoh <masanori.itoh@gmail.com>
2021-10-31 01:05:57 +0900

    Minor update Nd4jTest1.java

--- java/Nd4jTest1/src/main/java/com/github/thatsdone/junkbox/Nd4jTest1.java
+++ java/Nd4jTest1/src/main/java/com/github/thatsdone/junkbox/Nd4jTest1.java
@@ -13,13 +13,21 @@ import org.nd4j.linalg.api.ndarray.BaseNDArray;
 import org.nd4j.linalg.api.ndarray.INDArray;
 import org.nd4j.linalg.factory.Nd4j;
 import java.util.Arrays;
+import org.nd4j.linalg.api.buffer.DataType;
 
 public class Nd4jTest1
 {
     public static void main(String[] args) throws Exception
     {
-	INDArray m = Nd4j.zeros(3, 3);
-
+	int[] shape = {1, 3};
+	/**
+	INDArray m = Nd4j.zeros(shape, DataType.DOUBLE);
+	System.out.printf("m:\n%s\n", m);
+	m = Nd4j.vstack(m, Nd4j.zeros(shape,  DataType.DOUBLE));
+	System.out.printf("m:\n%s\n", m);
+	m = Nd4j.vstack(m, Nd4j.zeros(shape,  DataType.DOUBLE));
+	*/
+	INDArray m = Nd4j.zeros(new int[] {3, 3}, DataType.DOUBLE);
 	System.out.printf("m:\n%s\n", m);
 	for (int i = 0; i < 3; i++) {
 	    for (int j = 0; j < 3; j++) {
@@ -33,10 +41,16 @@ public class Nd4jTest1
 	//double val2[] = {1.234, 2.345, 3.456};
 	//m.addRow(val2);
 	
-	m.putRow(m.rows() + 1, Nd4j.create(new double[] {1.234, 2.345, 3.456} ));
-	//INDArray v = Nd4j.vstack(m, Nd4j.create(new double[] {1.234, 2.345, 3.456})); //, new int[]{3, 1}));
-	//m.addRowVector(Nd4j.create(new double[] {1.234, 2.345, 3.456})); //, new int[]{3, 1}));
+	//m.putRow(m.rows() + 1, Nd4j.create(new double[] {1.234, 2.345, 3.456} ));
+	INDArray newrow = Nd4j.create(new double[] {1.234, 2.345, 3.456}, new long[] {1,3}, DataType.DOUBLE);
+	System.out.printf("newrow:\n%s\n", newrow);
+	INDArray v = Nd4j.vstack(m, newrow);
+
+	//below works!?
+	//INDArray v = Nd4j.vstack(m, Nd4j.zeros(1, 3));
+
+	//m.addRowVector(Nd4j.create(new double[] {1.234, 2.345, 3.456}));
 	//INDArray c = Nd4j.concat(0, m, Nd4j.create(new double[] {1.234, 2.345, 3.456}));
-	System.out.printf("m:\n%s\n", m);
+	System.out.printf("m:\n%s\n", v);
     }
 }
