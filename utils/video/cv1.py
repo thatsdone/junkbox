e4f68416240d0c3eab4ac192ac75ed22a709b2fc
Masanori Itoh <masanori.itoh@gmail.com>
2022-01-16 23:46:36 +0900

    Update utils/video/cv1.py

--- utils/video/cv1.py
+++ utils/video/cv1.py
@@ -17,7 +17,7 @@ cap = cv2.VideoCapture(video_file)
 print('WIDTH  %d' % (cap.get(cv2.CAP_PROP_FRAME_WIDTH)))
 print('HEIGHT %d' % (cap.get(cv2.CAP_PROP_FRAME_HEIGHT)))
 print('FPS    %d' % (cap.get(cv2.CAP_PROP_FPS)))
-print('COUNT  %d' % (cap.get(cv2.CAP_PROP_FRAME_COUNT)))
+print('FRAME_COUNT  %d' % (cap.get(cv2.CAP_PROP_FRAME_COUNT)))
 fcc = int(cap.get(cv2.CAP_PROP_FOURCC))
 print('FOURCC %c%c%c%c (0x%08x)' % (
     (fcc & 0xff),
@@ -26,5 +26,20 @@ print('FOURCC %c%c%c%c (0x%08x)' % (
     ((fcc >> 24) & 0xff),
     fcc)
 )
+# On CAP_PROP_FORMAT
+# https://stackoverflow.com/questions/46094985/what-do-the-numbers-returned-by-opencvs-videocapturegetcv-cap-prop-format-m
+# https://stackoverflow.com/questions/14364563/minmaxloc-datatype/14365886#14365886
+print('FORMAT %d' % (cap.get(cv2.CAP_PROP_FORMAT)))
+# backend specific value indicating the current capture mode
+print('MODE  %d' % (cap.get(cv2.CAP_PROP_MODE)))
+print('GAMMA %s' % (cap.get(cv2.CAP_PROP_GAMMA)))
+print('TEMPERATUR %s' % (cap.get(cv2.CAP_PROP_TEMPERATURE)))
+print('SETTINGS %s' % (cap.get(cv2.CAP_PROP_SETTINGS)))
+print('CONVERT_RGB %s' % (cap.get(cv2.CAP_PROP_CONVERT_RGB)))
+print('POS_MSEC %s' % (cap.get(cv2.CAP_PROP_POS_MSEC)))
 
 cap.release()
+#
+# other references
+#
+# https://ichi.pro/opencv-python-gazo-to-bideo-no-yomitori-to-kakikomi-260590272275980
