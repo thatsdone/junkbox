9bf436aa56540d28c68909751cb2229d6432c036
Masanori Itoh <masanori.itoh@gmail.com>
2022-02-09 23:12:46 +0900

    Add java/OtelTest1

---
+++ java/OtelTest1/src/main/java/com/github/thatsdone/junkbox/App.java
@@ -0,0 +1,13 @@
+package com.github.thatsdone.junkbox;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
