4e18af601428a0f699e0480dd2c43fd12ea533e8
Masanori Itoh <masanori.itoh@gmail.com>
2022-02-19 17:04:42 +0900

    User numpy in random1.py

--- python/random1.py
+++ python/random1.py
@@ -4,50 +4,63 @@
 #
 # https://stackoverflow.com/questions/38273353/how-to-repeat-individual-characters-in-strings-in-python/38273369
 #
-import random
+#import random
 import sys
+import numpy as np
+from scipy import stats as st
+#rng = np.random.RandomState(10)
 
 
-print('0.0<=float<1.0 10<=float<20  100<=int<= 1000  100<=even int.< 1000')
-for i in range(5):
-    #print '| random float: 0.0 <= number < 1.0'
-    print('%15.13f' %  random.random())
-    #print('| random float: 10 <= number < 20'
-    print('%16.14f' % random.uniform(10, 20))
-    #print('| random integer: 100 <= number <= 1000'
-    print('%5d' % random.randint(100, 1000))
-    #print('| random integer: even numbers in 100 <= number < 1000'
-    print('%5d' % random.randrange(100, 1000, 2))
-
-print('')
-
-histogram = [0] * 20 
-histogram2 = [0] * 20 
-for i in range(1000):
-    j = int(random.gauss(5, 1) * 2)
-    k = int(random.uniform(0, 10) * 2)
-    histogram[j] = histogram[j] + 1
-    histogram2[k] = histogram2[k] + 1
-
-print('Generate gaussian random number sequence and show histogram picture')
-m = max(histogram)
-for v in histogram:
-    print('| %s' % (''.join(map(lambda x: x * int(v * 50 / m), "*"))))
-
-print('Generate uniform and gaussian random number sequences and show histogram')
-for v in range(0, 20):
-    print('%4.1f - %4.1f : %5d %5d' % (float(v) / 2, (float(v) + 1) / 2, histogram[v], histogram2[v]))
+def draw_hist(hist, column_width=50):
+    elm_max = np.ceil(max(hist[0]))
+    elm_min = np.ceil(min(hist[0]))
+    nbins=len(hist[1]) - 1
+    step=hist[1][1] - hist[1][0]
+    print('==================')
+    for i in range(0, nbins):
+        print('%8.2f - %8.2f : %s' % (float(hist[1][i]),
+                                      float(hist[1][i]+step),
+                                      ''.join(map(lambda x: x * int(hist[0][i] * column_width / elm_max), "*"))))
+    print('------------------')
 
 
+if __name__ == "__main__":
+    count = 10000
+    if len(sys.argv) > 1:
+        count = int(sys.argv[1])
+    print('random1.py: random number genreration and histgoram sample')
+    print('count = %d' % (count))
 
-#random.seed()
-#
-#count = 0
-#while True:
-#    if count > 100:
-#        sys.exit(0)
-#    print(random()
-#    print(uniform(0,10)
+    #normal
+    avg = 5
+    stddev = 1
+    rand_normal  = np.zeros(count, dtype=np.float64)
 
+    #uniform
+    low = 0
+    high = 10
+    rand_uniform = np.zeros(count, dtype=np.float64)
 
+    #exponential
+    scale = 2.0
+    size = 10
+    rand_exponential = np.zeros(count, dtype=np.float64)
+    
+    for i in range(count):
+        rand_normal[i] = np.random.normal(loc=avg, scale=stddev)
+        rand_uniform[i] = np.random.uniform(low=low, high=high)
+        rand_exponential[i] = np.random.exponential(scale=scale)
+        
+    print('normal    : avg = %f stdev = %f' % (avg, stddev))
+    hist_normal = np.histogram(rand_normal, bins=20, range=(0,10))
+    draw_hist(hist_normal)
+
+    print('uniform   : low = %f high  = %f' % (low, high))
+    hist_uniform = np.histogram(rand_uniform, bins=20,range=(0,10))
+    draw_hist(hist_uniform)
+
+    #print(rand_exponential)
+    print('exponential : scale (1/lambda) = %f' % (scale))
+    hist_exponential = np.histogram(rand_exponential, bins=20, range=(0,10))
+    draw_hist(hist_exponential)
 
