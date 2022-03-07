73a027399e794264a41bb42034b6d1e12a728044
Masanori Itoh <masanori.itoh@gmail.com>
2022-03-07 22:00:12 +0900

    Add python/kafkacli.py

---
+++ python/kafkacli.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# kafkacli.py: A tiny kafka topic status checker
+#
+# License:
+#   Apache License, Version 2.0
+#
+# History:
+#   * 2022/03/07 v0.1 Initial version
+# Author:
+#   Masanori Itoh <masanori.itoh@gmail.com>
+#
+# Dependencies:
+#   * kafka-pytyon : https://kafka-python.readthedocs.io/
+#
+from kafka import KafkaConsumer
+from kafka import KafkaClient
+from kafka.structs import TopicPartition
+from kafka.structs import PartitionMetadata
+from kafka.cluster import ClusterMetadata
+
+import time
+import argparse
+#import logging
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='kafkacli.py')
+    parser.add_argument('-t', '--topic', default='my-topic')
+    parser.add_argument('-b', '--bootstrap_servers', default='localhost:9092')
+    parser.add_argument('-i', '--interval', default=5)
+    parser.add_argument('--debug', action='store_true')
+    parser.add_argument('--logfile', default='pygen.log')
+    args = parser.parse_args()
+    #
+    debug = False
+    if args.debug:
+        debug = args.debug
+    topic = args.topic
+    bootstrap_servers = args.bootstrap_servers
+    interval = int(args.interval)
+
+    print('# bootstrap_servers: %s' % (bootstrap_servers))
+    print('# topic: %s' % (topic))
+    print('# interval: %d' % (interval))
+
+    #
+    consumer = KafkaConsumer( bootstrap_servers=bootstrap_servers)
+    topics = consumer.topics()
+    #
+    topic_partitions = consumer.partitions_for_topic(topic)
+    tps=[]
+    for elm in topic_partitions:
+        tp=TopicPartition(topic=topic, partition=elm)
+        tps.append(tp)
+    #
+    consumer.assign(tps)
+    #
+    consumer.seek_to_beginning()
+    #
+    beginning_offsets = consumer.beginning_offsets(tps)
+    if debug:
+        print('# topic: %s part ealiest lastest msg' % (tp.topic))
+        for tp in tps:
+            print('  %d %d %d %d' % (tp.partition,
+                                     consumer.position(tp),
+                                     beginning_offsets[tp],
+                                     consumer.position(tp) -
+                                     beginning_offsets[tp]))
+
+    print('# timestamp topic partitions msgs_in_topic')
+    interval = 5
+    while True:
+        num_msgs = 0
+        for tp in tps:
+            num_msgs += (consumer.position(tp) - beginning_offsets[tp])
+
+        print('%s %s %d %d' % (time.strftime("%Y/%m/%d %H:%M:%S",
+                                             time.localtime()),
+                               topic, len(tps), num_msgs))
+        time.sleep(interval)
