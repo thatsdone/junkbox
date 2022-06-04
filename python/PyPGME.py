d96a8df899c96c0c17bb86d76483a61566af10b7
Masanori Itoh <masanori.itoh@gmail.com>
2022-06-04 10:17:14 +0900

    Make PyPGMEpy return HTTP/1.1 response.

--- python/PyPGME.py
+++ python/PyPGME.py
@@ -31,29 +31,39 @@ extended_attrs = True
 class PyPGME(BaseHTTPRequestHandler):
 
     def do_GET(self):
+        # FIXME(thatsdone): Are there appropriate initializers?
+        self.protocol_version = 'HTTP/1.1'
 
         # Check path element
         if not (self.path == '/metrics'):
+            msg = ('<html><head><title>PyPGME</title></head>\n'
+                   '<body>\n'
+                   '<h1>PyPGME</h1>\n'
+                   '<p><a href="/metrics"</a>Metrics<p>\n'
+                   '</body></html>\n')
             self.send_response(HTTPStatus.OK)
             self.send_header('Content-Type', 'text/html')
+            self.send_header('Content-Length', len(msg))
             self.end_headers()
-            self.wfile.write(('<html><head><title>PyPGME</title></head>\n'
-                              '<body>\n'
-                              '<h1>PyPGME</h1>\n'
-                              '<p><a href="/metrics"</a>Metrics<p>\n'
-                              '</body></html>\n').encode('utf-8'))
+            self.wfile.write(msg.encode('utf-8'))
             return
 
         self.send_response(HTTPStatus.OK)
         self.send_header('Content-Type', 'text/plain')
-        self.end_headers()
+        #self.end_headers()
 
         output = subprocess.Popen(command, stdin=subprocess.PIPE,
                                   stdout=subprocess.PIPE,
                                   close_fds=True).stdout
-        self.parse_nvidia_smi(output)
+        msg = self.parse_nvidia_smi(output)
+        self.send_header('Content-Length', len(msg))
+        self.end_headers()
+        self.wfile.write(msg.encode('utf-8'))
+        return
 
     def parse_nvidia_smi(self, output):
+
+        msg = ''
 
         for line in output:
             l = line.strip()
@@ -67,32 +77,32 @@ class PyPGME(BaseHTTPRequestHandler):
              power_draw, power_limit) = l.decode('utf-8').split(',')
 
             gpu_index = int(gpu_index)
-            self.wfile.write(("# TYPE temperature_gpu gauge\n").encode('utf-8'))
-            self.wfile.write(("# HELP temperature.gpu of nvidia-smi\n").encode('utf-8'))
-            self.wfile.write(("temperature_gpu{gpu=\"%s[%d]\"} %d\n" % (gpu_name, gpu_index, int(gpu_temp))).encode('utf-8'))
-            self.wfile.write(("# TYPE utilization_gpu gauge\n").encode('utf-8'))
-            self.wfile.write(("# HELP utilization.gpu of nvidia-smi\n").encode('utf-8'))
-            self.wfile.write(("utilization_gpu{gpu=\"%s[%d]\"} %d\n" % (gpu_name, gpu_index, int(gpu_util))).encode('utf-8'))
-            self.wfile.write(("# TYPE utilization_memory gauge\n").encode('utf-8'))
-            self.wfile.write(("# HELP utilization.memory of nvidia-smi\n").encode('utf-8'))
-            self.wfile.write(("utilization_memory{gpu=\"%s[%d]\"} %d\n" % (gpu_name, gpu_index, int(mem_util))).encode('utf-8'))
-            self.wfile.write(("# TYPE memory_total gauge\n").encode('utf-8'))
-            self.wfile.write(("# HELP memory.total of nvidia-smi\n").encode('utf-8'))
-            self.wfile.write(("memory_total{gpu=\"%s[%d]\"} %d\n" % (gpu_name, gpu_index, int(mem_total))).encode('utf-8'))
-            self.wfile.write(("# TYPE memory_free gauge\n").encode('utf-8'))
-            self.wfile.write(("# HELP memory.free of nvidia-smi\n").encode('utf-8'))
-            self.wfile.write(("memory_free{gpu=\"%s[%d]\"} %d\n" % (gpu_name, gpu_index, int(mem_free))).encode('utf-8'))
-            self.wfile.write(("# TYPE memory_used gauge\n").encode('utf-8'))
-            self.wfile.write(("# HELP memory.used of nvidia-smi\n").encode('utf-8'))
-            self.wfile.write(("memory_used{gpu=\"%s[%d]\"} %d\n" % (gpu_name, gpu_index, int(mem_used))).encode('utf-8'))
+            msg += "# TYPE temperature_gpu gauge\n"
+            msg += "# HELP temperature.gpu of nvidia-smi\n"
+            msg += "temperature_gpu{gpu=\"%s[%d]\"} %d\n" % (gpu_name, gpu_index, int(gpu_temp))
+            msg += "# TYPE utilization_gpu gauge\n"
+            msg += "# HELP utilization.gpu of nvidia-smi\n"
+            msg += "utilization_gpu{gpu=\"%s[%d]\"} %d\n" % (gpu_name, gpu_index, int(gpu_util))
+            msg += "# TYPE utilization_memory gauge\n"
+            msg += "# HELP utilization.memory of nvidia-smi\n"
+            msg += "utilization_memory{gpu=\"%s[%d]\"} %d\n" % (gpu_name, gpu_index, int(mem_util))
+            msg += "# TYPE memory_total gauge\n"
+            msg += "# HELP memory.total of nvidia-smi\n"
+            msg += "memory_total{gpu=\"%s[%d]\"} %d\n" % (gpu_name, gpu_index, int(mem_total))
+            msg += "# TYPE memory_free gauge\n"
+            msg += "# HELP memory.free of nvidia-smi\n"
+            msg += "memory_free{gpu=\"%s[%d]\"} %d\n" % (gpu_name, gpu_index, int(mem_free))
+            msg += "# TYPE memory_used gauge\n"
+            msg += "# HELP memory.used of nvidia-smi\n"
+            msg += "memory_used{gpu=\"%s[%d]\"} %d\n" % (gpu_name, gpu_index, int(mem_used))
             if extended_attrs:
-                self.wfile.write(("# TYPE pgme_power_draw gauge\n").encode('utf-8'))
-                self.wfile.write(("# HELP power.draw of nvidia-smi\n").encode('utf-8'))
-                self.wfile.write(("pgme_power_draw{gpu=\"%s[%d]\"} %s\n" % (gpu_name, gpu_index, power_draw)).encode('utf-8'))
-                self.wfile.write(("# TYPE pgme_power_limit gauge\n").encode('utf-8'))
-                self.wfile.write(("# HELP power.limit of nvidia-smi\n").encode('utf-8'))
-                self.wfile.write(("pgme_power_limit{gpu=\"%s[%d]\"} %s\n" % (gpu_name, gpu_index, power_limit)).encode('utf-8'))
-        return
+                msg += "# TYPE pgme_power_draw gauge\n"
+                msg += "# HELP power.draw of nvidia-smi\n"
+                msg += "pgme_power_draw{gpu=\"%s[%d]\"} %s\n" % (gpu_name, gpu_index, power_draw)
+                msg += "# TYPE pgme_power_limit gauge\n"
+                msg += "# HELP power.limit of nvidia-smi\n"
+                msg += "pgme_power_limit{gpu=\"%s[%d]\"} %s\n" % (gpu_name, gpu_index, power_limit)
+        return msg
 #
 #
 #
