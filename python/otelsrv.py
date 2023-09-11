a01818ea89bf8daef8725c9083b372be7667b0bd
Masanori Itoh <masanori.itoh@gmail.com>
2023-09-11 21:45:07 +0900

    python/otelsrv.py: Add --enable_otel option

--- python/otelsrv.py
+++ python/otelsrv.py
@@ -23,14 +23,6 @@ from http.server import BaseHTTPRequestHandler
 from http.server import HTTPServer
 from http import HTTPStatus
 #
-from opentelemetry.sdk.resources import Resource
-from opentelemetry.propagate import inject
-from opentelemetry import trace
-from opentelemetry.sdk.trace import TracerProvider
-from opentelemetry.sdk.trace.export import BatchSpanProcessor
-from opentelemetry.sdk.trace.export import ConsoleSpanExporter
-from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import OTLPSpanExporter
-from opentelemetry.trace.propagation.tracecontext import TraceContextTextMapPropagator
 
 tracer = None
 args = None
@@ -73,22 +65,26 @@ class OtelSrv(BaseHTTPRequestHandler):
         self.end_headers()
         self.wfile.write(msg.encode('utf-8'))
 
-        
-        with tracer.start_as_current_span("do_POST", context=ctx) as span:
+        span1 = None
+        if args.enable_otel:
+            span1 = tracer.start_span("do_POST", context=ctx)
             inject(headers)
-            print(headers['traceparent'])
-            if len(post_data) > args.dump_size:
-                dump_size = args.dump_size
-            else:
-                dump_size = len(post_data)
-            dump_str = ''
-            for i in range(0, dump_size):
-                dump_str += '%02x ' % (post_data[i])
-                if ((i + 1) % 16) == 0:
-                    print(dump_str)
-                    dump_str = ''
+            if 'traceparent' in headers.keys():
+                print(headers['traceparent'])
+        if len(post_data) > args.dump_size:
+            dump_size = args.dump_size
+        else:
+            dump_size = len(post_data)
+        dump_str = ''
+        for i in range(0, dump_size):
+            dump_str += '%02x ' % (post_data[i])
+            if ((i + 1) % 16) == 0:
+                print(dump_str)
+                dump_str = ''
             if (dump_size % 16 != 0):
                 print(dump_str)
+        if args.enable_otel:
+            span1.end()
 
 
 if __name__ == "__main__":
@@ -100,22 +96,32 @@ if __name__ == "__main__":
     parser.add_argument('--bind_address', default='0.0.0.0')
     parser.add_argument('--bind_port', type=int, default=8080)
     parser.add_argument('--dump_size', type=int, default=256)
+    parser.add_argument('--enable_otel', action='store_true')
     args = parser.parse_args()
     #
     # Setup OpenTelemetry
     #
-    resource = Resource(attributes={'service.name': sys.argv[0]})
-    provider = TracerProvider(resource=resource)
-    trace.set_tracer_provider(provider)
-    tracer = trace.get_tracer(sys.argv[0])
-    if args.endpoint:
-        otlp_exporter = OTLPSpanExporter(endpoint=args.endpoint, insecure=True)
-        otlp_processor = BatchSpanProcessor(otlp_exporter)
-        trace.get_tracer_provider().add_span_processor(otlp_processor)
-    if args.console:
-        console_exporter = ConsoleSpanExporter()
-        console_processor = BatchSpanProcessor(console_exporter)
-        trace.get_tracer_provider().add_span_processor(console_processor)
+    if args.enable_otel:
+        from opentelemetry.sdk.resources import Resource
+        from opentelemetry.propagate import inject
+        from opentelemetry import trace
+        from opentelemetry.sdk.trace import TracerProvider
+        from opentelemetry.sdk.trace.export import BatchSpanProcessor
+        from opentelemetry.sdk.trace.export import ConsoleSpanExporter
+        from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import OTLPSpanExporter
+        from opentelemetry.trace.propagation.tracecontext import TraceContextTextMapPropagator
+        resource = Resource(attributes={'service.name': sys.argv[0]})
+        provider = TracerProvider(resource=resource)
+        trace.set_tracer_provider(provider)
+        tracer = trace.get_tracer(sys.argv[0])
+        if args.endpoint:
+            otlp_exporter = OTLPSpanExporter(endpoint=args.endpoint, insecure=True)
+            otlp_processor = BatchSpanProcessor(otlp_exporter)
+            trace.get_tracer_provider().add_span_processor(otlp_processor)
+        if args.console:
+            console_exporter = ConsoleSpanExporter()
+            console_processor = BatchSpanProcessor(console_exporter)
+            trace.get_tracer_provider().add_span_processor(console_processor)
     #
     # Create HTTP Server
     #
