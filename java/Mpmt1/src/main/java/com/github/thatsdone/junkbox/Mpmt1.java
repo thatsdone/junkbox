553c9dc260497950256f6f5ecd354fe3277add21
Masanori Itoh <masanori.itoh@gmail.com>
2023-10-09 10:38:00 +0900

    java/Mpmt1: Add some system information output and fix a typo

--- java/Mpmt1/src/main/java/com/github/thatsdone/junkbox/Mpmt1.java
+++ java/Mpmt1/src/main/java/com/github/thatsdone/junkbox/Mpmt1.java
@@ -12,6 +12,7 @@ package com.github.thatsdone.junkbox;
  */
 import java.util.*;
 import java.io.*;
+import java.lang.Runtime;
 
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.CommandLineParser;
@@ -35,8 +36,11 @@ public class Mpmt1
 
         @Override
         public void run() {
-	    System.out.println("worker::run():  index = " + index);
 	    long ts_orig = System.currentTimeMillis();
+	    System.out.println("worker::run(): ts: " + ts_orig
+			       + " index = " + index
+			       + " isVirtual: "
+			       + Thread.currentThread().isVirtual());
             long ts;
 	    int count = 0;
             while (true) {
@@ -46,6 +50,8 @@ public class Mpmt1
                     return;
                 }
                 count++;
+		//This does not have any effect for Virtual Thread?
+		//Thread.currentThread().yield();
             }
         }
     }
@@ -85,9 +91,21 @@ public class Mpmt1
         if (cmd.hasOption("v")) {
 	    vthread = true;
 	}
-        if (cmd.hasOption("v")) {
+        if (cmd.hasOption("s")) {
 	    sleep = Integer.parseInt(cmd.getOptionValue("s"));
 	}
+
+	String vp = System.getProperty("jdk.virtualThreadScheduler.parallelism");
+	String vt = System.getProperty("jdk.virtualThreadScheduler.maxPoolSize");
+	String mr = System.getProperty("jdk.virtualThreadScheduler.minRunnable");
+	System.out.printf("parallelism: %s maxPollSize: %s minRunnable: %s\n",
+			  vp, vt, mr);
+	System.out.printf("availableProcessors: %d maxMemory: %d totalMemory: %d freeMemory: %d\n",
+			  Runtime.getRuntime().availableProcessors(),
+			  Runtime.getRuntime().maxMemory(),
+			  Runtime.getRuntime().totalMemory(),
+			  Runtime.getRuntime().freeMemory()
+			  );
 
         for (i = 0; i < num_context; i++) {
 	    worker w = new worker(i, duration * 1000L);
@@ -104,6 +122,7 @@ public class Mpmt1
 		t.start();
 	    }
         }
+
 	if (vthread) {
 	    System.out.println("Virtual Thread mode. Sleeping...");
 	    Thread.sleep(sleep * 1000L);
